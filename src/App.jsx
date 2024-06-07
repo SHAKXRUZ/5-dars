d194b5a5424c41bde215267b4236f282a6f3ffb6
shakhruz <ismoilovshaxruz775@gmail.com>
2024-06-07 15:43:21 +0500

    safdsdghj

--- src/App.jsx
+++ src/App.jsx
@@ -1,107 +1,44 @@
 import "./App.css";
-// import Routes from "./routes/Routes";
 
-// import { Link } from "react-router-dom";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-
-import { useRef } from "react";
-import "swiper/css";
-import "swiper/css/effect-cube";
-import "swiper/css/pagination";
-
-import Car from "./assets/images/car.jpg";
-
-import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { useState } from "react";
 
 function App() {
-  const progressCircle = useRef(null);
-  const progressContent = useRef(null);
-  const onAutoplayTimeLeft = (s, time, progress) => {
-    progressCircle.current.style.setProperty("--progress", 1 - progress);
-    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
-  };
+  const [acardion1, setAcardion1] = useState(false);
+  const [acardion2, setAcardion2] = useState(false);
+  const [acardion3, setAcardion3] = useState(false);
+
+  function func1() {
+    setAcardion1(true);
+    setAcardion2(false);
+    setAcardion3(false);
+  }
+
+  function func2() {
+    setAcardion1(false);
+    setAcardion2(true);
+    setAcardion3(false);
+  }
+
+  function func3() {
+    setAcardion1(false);
+    setAcardion2(false);
+    setAcardion3(true);
+  }
+
   return (
-    // <header className="header">
-    //   <nav className="navbar">
-    //     <Link to="/">Home</Link>
-    //     <Link to="/shifokor">Shifokor</Link>
-    //     <Link to="/news">News</Link>
-    //     <Link to="/section">Section</Link>
-    //     <Link to="/contact">Contact</Link>
-    //   </nav>
-
-    //   <Routes />
-    // </header>
-
-    <div className="row">
-      <Swiper
-        spaceBetween={100}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        onAutoplayTimeLeft={onAutoplayTimeLeft}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <div className="box">
-            <img src={Car} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="box">
-            <img src={Car} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="box">
-            <img src={Car} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="box">
-            <img src={Car} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="box">
-            <img src={Car} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="box">
-            <img src={Car} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="box">
-            <img src={Car} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="box">
-            <img src={Car} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="box">
-            <img src={Car} alt="" />
-          </div>
-        </SwiperSlide>
-        <div className="autoplay-progress" slot="container-end">
-          <svg viewBox="0 0 48 48" ref={progressCircle}>
-            <circle cx="24" cy="24" r="20"></circle>
-          </svg>
-          <span ref={progressContent}></span>
-        </div>
-      </Swiper>
+    <div className="acardion">
+      <div className="box">
+        <p onClick={() => func1()}>Lorem ipsum dolor sit amet.</p>
+        {acardion1 === true ? <p className="row">dfghgjkhjghfgdf</p> : null}
+      </div>
+      <div className="box">
+        <p onClick={() => func2()}>Lorem ipsum dolor sit amet.</p>
+        {acardion2 === true ? <p className="row">Lorem, ipsum dolor.</p> : null}
+      </div>
+      <div className="box">
+        <p onClick={() => func3()}>Lorem ipsum dolor sit amet.</p>
+        {acardion3 === true ? <p className="row">Lorem, ipsum dolor.</p> : null}
+      </div>
     </div>
   );
 }
